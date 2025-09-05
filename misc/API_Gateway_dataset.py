40468213ee85d7a8ef628cdd28a11487971ddd03
Sarthak Mohapatra <sarthak.mohapatra@veersatech.com>
2025-09-05 15:06:07 +0530

    Initial commit: add IsolationForest script and dataset

---
+++ misc/API_Gateway_dataset.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+np.random.seed(32)
+n=50000
+df=pd.DataFrame({
+"endpoint":np.random.choice(["/login","/purchase","/get-user","/matrics"],n),
+"method":np.random.choice(["GET","POST"],n),
+"payload_size":np.random.randint(50,2000,n),
+"response_time":np.random.randint(n)*2,
+"status_code":np.random.choice([200,400,401,404,500],n,p=[0.9,0.02,0.02,0.03,0.03])
+})
+index=np.random.choice(df.index,1000,replace=False)
+df.loc[index,"payload_size"]*=20
+df.loc[index,"response_time"]*=10
+df.to_csv("api_dataset.csv",index=False)
---
+++ misc/API_Gateway_modelling.py
@@ -0,0 +1,23 @@
+from sklearn.ensemble import IsolationForest
+import pandas as pd
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.pipeline import Pipeline 
+df=pd.read_csv(r"C:\Users\SarthakMohapatra\Desktop\sarthak_dev_code\misc\api_dataset.csv")
+iso=IsolationForest(
+    n_estimators=500,
+    contamination=0.1,
+    random_state=42
+)
+num_cols=df.select_dtypes(include=["float64","Int64","float32","Int32"]).columns.to_list()
+cat_cols=df.select_dtypes(include=["object","category"]).columns.to_list()
+preprocessor=ColumnTransformer(transformers=[
+    ("cat",OneHotEncoder(handle_unknown="ignore"),cat_cols),
+    ("num",StandardScaler(),num_cols)
+
+])
+X = preprocessor.fit_transform(df)
+iso.fit(X)
+preds=iso.predict(X)
+print(preds)
