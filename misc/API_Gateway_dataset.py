3f37836a3bc6fd2c08181999cbce2011c06fbe8b
Sarthak Mohapatra <140512881+sarthakm402@users.noreply.github.com>
2026-03-22 16:07:57 +0530

    Fix formatting issues in API_Gateway_dataset.py

--- misc/API_Gateway_dataset.py
+++ misc/API_Gateway_dataset.py
@@ -6,7 +6,7 @@ df=pd.DataFrame({
 "endpoint":np.random.choice(["/login","/purchase","/get-user","/matrics"],n),   
 "method":np.random.choice(["GET","POST"],n), 
 "payload_size":np.random.randint(50,2000,n),     
-"response_time":np.random.randint(50,5000,n),
+"response_time":np.random.randint(50,5000,n), 
 "status_code":np.random.choice([200,400,401,404,500],n,p=[0.9,0.02,0.02,0.03,0.03])
 }) 
 index=np.random.choice(df.index,1000,replace=False)
